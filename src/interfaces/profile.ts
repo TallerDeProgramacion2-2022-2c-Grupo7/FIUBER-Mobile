c384f7b5b4ad7ecf190c118445cd3fc25e53fe05
aleacevedo <ale.acevedo@live.com.ar>
2022-09-30 11:08:59 -0300

    feat: add profiles logic to all other screens

---
+++ src/interfaces/profile.ts
@@ -0,0 +1,22 @@
+interface DriverProfile {
+  isDriver?: true;
+  car: {
+    brand: string;
+    model: string;
+    color: string;
+    plate: string;
+  };
+}
+
+interface PassengerProfile {
+  isDriver?: false;
+}
+export interface CommonsProfile {
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: string;
+  verifiedPhone: boolean;
+}
+
+export type Profile = CommonsProfile & (DriverProfile | PassengerProfile);
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,44 +1,46 @@
 import type { NativeStackScreenProps } from '@react-navigation/native-stack';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Text, View } from 'react-native';
+import MapView, { Marker, PROVIDER_GOOGLE } from 'react-native-maps';
 import { useDispatch, useSelector } from 'react-redux';
 
-import Button from '../../components/Button';
 import { ROUTES } from '../../constants/routes';
 import { RootStackParamList } from '../../interfaces/navigation';
 import { AppDispatch, ReduxState } from '../../interfaces/redux';
 import { Container } from '../../layouts';
-import { logout } from '../../redux/slices/auth';
-
-import {StyleSheet} from 'react-native';
-import MapView, {PROVIDER_GOOGLE, Marker} from 'react-native-maps';
-
 import styles from './styles';
-
-
-
 
 type Props = NativeStackScreenProps<RootStackParamList, ROUTES.HOME_SCREEN>;
 
 function Home({ navigation }: Props) {
   const { t } = useTranslation();
   const dispatch = useDispatch<AppDispatch>();
-  const { user } = useSelector((state: ReduxState) => state.auth);
+  const { logedIn } = useSelector((state: ReduxState) => state.auth);
+  const { obtained: profileObtained } = useSelector(
+    (state: ReduxState) => state.profile
+  );
+
+  useEffect(() => {
+    if (!logedIn) {
+      navigation.navigate(ROUTES.LOGIN_SCREEN);
+      return;
+    }
+    if (logedIn && !profileObtained) {
+      navigation.navigate(ROUTES.SET_PROFILE_SCREEN);
+    }
+  }, [logedIn, profileObtained]);
 
   return (
     <Container>
-		<MapView
-
-		  style={styles.map}
-
-		  initialRegion={{
-		    latitude: 37.78825,
-		    longitude: -122.4324,
-		    latitudeDelta: 0.0922,
-		    longitudeDelta: 0.0421,
-		  }}
-		/>
+      <MapView
+        style={styles.map}
+        initialRegion={{
+          latitude: 37.78825,
+          longitude: -122.4324,
+          latitudeDelta: 0.0922,
+          longitudeDelta: 0.0421,
+        }}
+      />
     </Container>
   );
 }
