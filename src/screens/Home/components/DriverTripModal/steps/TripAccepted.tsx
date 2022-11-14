f07a735cd599da362bc0e2c6c6ba82a0a7149a05
0xAaCE <aacevedo@fi.uba.ar>
2022-11-14 19:36:37 -0300

    feat: implement on the move (#27)
    * feat: add on the move attr to redux slice
    
    * feat: implement follow user when on the move is setted

--- src/screens/Home/components/DriverTripModal/steps/TripAccepted.tsx
+++ src/screens/Home/components/DriverTripModal/steps/TripAccepted.tsx
@@ -1,14 +1,22 @@
 import React, { useEffect } from 'react';
 import { View } from 'react-native';
 import { Bar as ProgressBarr } from 'react-native-progress';
+import { useDispatch } from 'react-redux';
 
 import Header from '../../../../../components/Header';
 import Text from '../../../../../components/Text';
+import { AppDispatch } from '../../../../../interfaces/redux';
+import { setOnTheMove } from '../../../../../redux/slices/trip';
 import { IModalComponentArgs } from '..';
 
 const TripAccepted = ({ setAllwaysOpen }: IModalComponentArgs) => {
+  const dispatch = useDispatch<AppDispatch>();
   useEffect(() => {
     setAllwaysOpen(85);
+  }, []);
+
+  useEffect(() => {
+    dispatch(setOnTheMove(true));
   }, []);
 
   return (
