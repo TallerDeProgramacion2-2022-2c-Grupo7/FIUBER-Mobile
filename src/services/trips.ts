f0a44707ac70c52e1b5ce0cb7207f1182a13c105
aleacevedo <ale.acevedo@live.com.ar>
2022-11-15 11:29:52 -0300

    feat: add some utils and improve services

--- src/services/trips.ts
+++ src/services/trips.ts
@@ -1,10 +1,13 @@
 import axios from 'axios';
 
 import { Trip, TripCordinates, TripPoints } from '../interfaces/trip';
+import { getFirebaseToken } from '../utils/firebase';
 
 const ENDPOINT = 'https://fiuber-trips-aleacevedo.cloud.okteto.net/api';
 
-export const calculateCost = async (trip: TripCordinates, token: string) => {
+export const calculateCost = async (trip: TripCordinates) => {
+  const token = await getFirebaseToken();
+
   try {
     const { data } = await axios.post(`${ENDPOINT}/costs/calculate`, trip, {
       headers: {
@@ -23,10 +26,9 @@ export const calculateCost = async (trip: TripCordinates, token: string) => {
   }
 };
 
-export const createTrip = async (
-  trip: TripPoints,
-  token: string
-): Promise<Trip> => {
+export const createTrip = async (trip: TripPoints): Promise<Trip> => {
+  const token = await getFirebaseToken();
+
   const { data } = await axios.post(`${ENDPOINT}/trips`, trip, {
     headers: {
       Authorization: `${token}`,
@@ -35,7 +37,9 @@ export const createTrip = async (
   return data.result;
 };
 
-export const getAvailable = async (token: string) => {
+export const getAvailable = async () => {
+  const token = await getFirebaseToken();
+
   const { data } = await axios.get(`${ENDPOINT}/trips/available`, {
     headers: {
       Authorization: `${token}`,
@@ -44,7 +48,9 @@ export const getAvailable = async (token: string) => {
   return data.result;
 };
 
-export const accept = async (id: string, token: string) => {
+export const accept = async (id: string) => {
+  const token = await getFirebaseToken();
+
   const { data } = await axios.post(
     `${ENDPOINT}/trips/${id}/accept`,
     {},
@@ -57,7 +63,24 @@ export const accept = async (id: string, token: string) => {
   return data.result;
 };
 
-export const getDriver = async (id: string, token: string) => {
+export const reject = async (id: string) => {
+  const token = await getFirebaseToken();
+
+  const { data } = await axios.post(
+    `${ENDPOINT}/trips/${id}/reject`,
+    {},
+    {
+      headers: {
+        Authorization: `${token}`,
+      },
+    }
+  );
+  return data.result;
+};
+
+export const getDriver = async (id: string) => {
+  const token = await getFirebaseToken();
+
   const { data } = await axios.get(`${ENDPOINT}/trips/${id}`, {
     headers: {
       Authorization: `${token}`,
@@ -66,7 +89,9 @@ export const getDriver = async (id: string, token: string) => {
   return data.result?.driverId;
 };
 
-export const getTripStatus = async (id: string, token: string) => {
+export const getTripStatus = async (id: string) => {
+  const token = await getFirebaseToken();
+
   try {
     const { data } = await axios.get(`${ENDPOINT}/trips/${id}`, {
       headers: {
@@ -80,7 +105,9 @@ export const getTripStatus = async (id: string, token: string) => {
   }
 };
 
-export const getTrip = async (id: string, token: string) => {
+export const getTrip = async (id: string) => {
+  const token = await getFirebaseToken();
+
   try {
     const { data } = await axios.get(`${ENDPOINT}/trips/${id}`, {
       headers: {
@@ -94,7 +121,24 @@ export const getTrip = async (id: string, token: string) => {
   }
 };
 
-export const finishTrip = async (id: string, token: string) => {
+export const start = async (id: string) => {
+  const token = await getFirebaseToken();
+
+  const { data } = await axios.post(
+    `${ENDPOINT}/trips/${id}/start`,
+    {},
+    {
+      headers: {
+        Authorization: `${token}`,
+      },
+    }
+  );
+  return data.result;
+};
+
+export const finishTrip = async (id: string) => {
+  const token = await getFirebaseToken();
+
   const { data } = await axios.post(
     `${ENDPOINT}/trips/${id}/finish`,
     {},
