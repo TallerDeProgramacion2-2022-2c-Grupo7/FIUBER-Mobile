34da4d9bbcace21589003c6fd41cfe49e8844067
Matías Huenul <43656633+matias-huenul@users.noreply.github.com>
2022-11-16 21:27:57 -0300

    Metrics API Integration (#28)

--- src/redux/slices/auth.ts
+++ src/redux/slices/auth.ts
@@ -4,6 +4,7 @@ import { GoogleSignin } from '@react-native-google-signin/google-signin';
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
 
 import { AuthLoginParams, AuthState } from '../../interfaces/redux';
+import { createEvent } from '../../services/metrics';
 
 const INITIAL_STATE: AuthState = {
   logedIn: false,
@@ -15,8 +16,10 @@ export const login = createAsyncThunk<any, AuthLoginParams>(
   'auth/login',
   async ({ email, password, onError }, {}) => {
     try {
-      await auth().signInWithEmailAndPassword(email, password);
-
+      const response = await auth().signInWithEmailAndPassword(email, password);
+      const uid = response.user.uid;
+      const token = await response.user.getIdToken();
+      createEvent('login', uid, token);
       return auth().currentUser?.toJSON();
     } catch (error: any) {
       console.error(error);
@@ -41,8 +44,10 @@ export const googleLogin = createAsyncThunk<any, void>(
       const googleCredential = auth.GoogleAuthProvider.credential(idToken);
 
       // Sign-in the user with the credential
-      await auth().signInWithCredential(googleCredential);
-
+      const response = await auth().signInWithCredential(googleCredential);
+      const uid = response.user.uid;
+      const token = await response.user.getIdToken();
+      createEvent('federatedLogin', uid, token);
       return auth().currentUser?.toJSON();
     } catch (error) {
       console.error(error);
@@ -55,8 +60,10 @@ export const signup = createAsyncThunk<any, AuthLoginParams>(
   'auth/signup',
   async ({ email, password, onError }, {}) => {
     try {
-      await auth().createUserWithEmailAndPassword(email, password);
-
+      const response = await auth().createUserWithEmailAndPassword(email, password);
+      const uid = response.user.uid;
+      const token = await response.user.getIdToken();
+      createEvent('signup', uid, token);
       return auth().currentUser?.toJSON();
     } catch (error: any) {
       console.error(error);
