752d8af1c35109b49749548481afc529fe607b58
Guido Bergman <gbergman@fi.uba.ar>
2022-11-18 12:11:37 -0300

    Fix rating service error

--- src/services/rating.ts
+++ src/services/rating.ts
@@ -2,22 +2,36 @@ import axios from 'axios';
 
 import { getFirebaseToken } from '../utils/firebase';
 
-const ENDPOINT = 'https://fiuber-ratings-guidobergman.cloud.okteto.net/';
+const ENDPOINT = 'https://fiuber-ratings-guidobergman.cloud.okteto.net';
 
 export const getRating = async (userId: string) => {
   const token = await getFirebaseToken();
-
+  try{
+   console.log('Will get rating ');
    const { data } = await axios.get(`${ENDPOINT}/${userId}/average`, {
      headers: {
        Authorization: `${token}`,
      },
    });
+   console.log('Got rating: ', data.result);
    return data.result;
+ }
+ catch (e) {
+    if (e.response) {
+      const { error } = e.response;
+      console.error('GET RATING ERROR WITH RESPONSE: ', e);
+      return undefined;
+    }
+ console.error('GET RATING ERROR: ', e.message);
+ return undefined;
+ }
 };
 
 export const addRating = async ( idTrip: string, idUserScored: string, value: number, idUserScorer: string) => {
   const token = await getFirebaseToken();
 
+  try{
+    console.log('WILL POST RATING ');
     const { data } = await axios.post(
       `${ENDPOINT}/`,
       {  id_trip: idTrip ,
@@ -32,5 +46,17 @@ export const addRating = async ( idTrip: string, idUserScored: string, value: nu
       }
     );
 
+     console.log('POST RATING: ', data.result);
      return data.result;
+ }
+ catch (e) {
+    if (e.response) {
+      const { error } = e.response;
+      console.error('POST RATING ERROR WITH RESPONSE: ', e);
+      console.error('POST RATING ERROR WITH RESPONSE: ', e.json());
+      return undefined;
+    }
+ console.error('POST RATING ERROR: ', e.message);
+ return undefined;
+ }
 };
